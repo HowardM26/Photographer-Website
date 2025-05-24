f30a29128075d6fdfeca28ae6c682c10096bd833
Howard Mell <mell_howard@yahoo.com>
2025-05-24 17:08:21 -0400

    same tab enter btn
    Changed the enter btn action to open the website in the same tab instead of opening it in a new tab when clicked.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header')
 let logoSpan = document.querySelectorAll('.logo');
+let enterBtn = document.getElementById('enterBtn');
+
 
 
 window.addEventListener('DOMContentLoaded', ()=>{
@@ -10,9 +12,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
         logoSpan.forEach((span, idx)=>{
             setTimeout(()=>{
                 span.classList.add('active');
-            }, (idx + 1) * 400)
-        })
+            }, (idx + 1) * 400);
+        });
+    });
 
-
-    })
-})
+    //Enter butt behavior
+    enterBtn.addEventListener('click', () => {
+        window.location.href = 'Homepage.html';
+    });
+});
